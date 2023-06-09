fe51297f57d356c2d31f68eefadc34c5e1f2fe98
Meryl Porsha <merylpor@un-k5.kzn.21-school.ru>
2023-06-09 21:39:13 +0300

    ex01 in process

--- src/day03/ex01/Program.java
+++ src/day03/ex01/Program.java
@@ -1,42 +1,78 @@
-//package day03.ex01;
-//
-//public class Program {
-//
-//  public static void main(String[] args) throws InterruptedException {
-//
-//    if (!args[0].startsWith("--count=") || args.length != 1) {
-//      System.err.println("Argument failed");
-//      System.exit(-1);
-//    }
-//    int count = Integer.parseInt((args[0].substring("--count=".length())));
-//    MyThread henThread = new MyThread(count, "Hen");
-//    MyThread eggThread = new MyThread(count, "Egg");
-//
-//    eggThread.start();
-//    henThread.start();
-//
-//    henThread.join();
-//    eggThread.join();
-//    for (int i = 0; i < count; ++i) {
-//      System.out.println("Human");
-//    }
-//  }
-//}
-//
-//class MyThread extends Thread {
-//
-//  int count;
-//  String result;
-//  MyThread(int count, String value) {
-//    this.count = count;
-//    this.result = value;
-//  }
-//
-//  public void run() {
-//    for (int i = 0; i < count; ++i) {
-//      System.out.println(this.result);
-//    }
-//  }
-//}
-//
-//
+package day03.ex01;
+
+public class Program {
+
+  public static void main(String[] args) throws InterruptedException {
+
+    if (!args[0].startsWith("--count=") || args.length != 1) {
+      System.err.println("Argument failed");
+      System.exit(-1);
+    }
+    int count = Integer.parseInt((args[0].substring("--count=".length())));
+    MyThread henThread = new MyThread(count, "Hen", false);
+    MyThread eggThread = new MyThread(count, "Egg", false);
+
+    eggThread.start();
+    henThread.start();
+
+  }
+}
+
+class MyThread extends Thread {
+
+  private boolean status = false;
+  int count;
+  String result;
+
+  MyThread(int count, String value, boolean status) {
+    this.count = count;
+    this.result = value;
+    this.status = status;
+  }
+
+  public boolean getStatus() {
+    return status;
+  }
+
+  public void setStatus(boolean value) {
+    this.status = value;
+  }
+
+  public void run() {
+    for (int i = 0; i < count; ++i) {
+      System.out.println(this.result);
+      if (status)
+        this.get();
+
+    }
+  }
+
+  public void get() {
+    while (!this.getStatus()) {
+      try {
+        this.setStatus(true);
+        wait();
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      this.setStatus(false);
+      notifyAll();
+    }
+  }
+
+  public void put() {
+    while (!this.getStatus()) {
+      try {
+        this.setStatus(false);
+        wait();
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      this.setStatus(true);
+      notifyAll();
+    }
+  }
+}
+
+
+
