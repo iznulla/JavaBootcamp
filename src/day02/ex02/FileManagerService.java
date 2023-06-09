c3aa6de1e184eadb2e44de6cd91eb86aab5577d6
Meryl Porsha <merylpor@un-k5.kzn.21-school.ru>
2023-06-09 14:31:34 +0300

    ex02 done

--- src/day02/ex02/FileManagerService.java
+++ src/day02/ex02/FileManagerService.java
@@ -5,17 +5,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class FileManagerService {
 
-  public Path _path;
+  private Path _path;
 
-  public FileManagerService() {
-    _path = Paths.get("/Users/merylpor/Desktop/JavaButcamp/src/day02");
+  public FileManagerService(String mainPath) {
+    _path = Paths.get(mainPath);
 
   }
 
-  public String cdCommand(String data) {
+  private String cdCommand(String data) {
     String resultPath = _path.toString();
     File dir = new File(_path.toString());
     dir = _path.resolve(data).normalize().toFile();
@@ -30,7 +31,7 @@ public class FileManagerService {
     return resultPath;
   }
 
-  public void lsCommand(String value) {
+  private void lsCommand(String value) {
     File folder = new File(value);
     try {
       for (File file : folder.listFiles()) {
@@ -45,7 +46,7 @@ public class FileManagerService {
   }
 
 
-  public void mvRename(String path, String from, String to) {
+  private void mvRename(String path, String from, String to) {
     File file = new File(path + "/" + from);
     File newFile = new File(file.getParent(), to);
     if (file.exists()) {
@@ -59,23 +60,42 @@ public class FileManagerService {
     }
   }
 
-  public void mvMove(String path, String filename, String to) {
+  private void mvMove(String path, String filename, String to) {
     try {
       Path source = Paths.get(path + "/" + filename);
       Path target = Paths.get(path);
       File dir = new File(_path.toString());
+
       dir  =_path.resolve(path).normalize().toFile();
       target = target.resolve(dir.getPath() + "/" + to + "/" + filename).normalize();
-      System.out.println(target);
-      Files.move(source, target);
 
+      Files.move(source, target);
     } catch (IOException e) {
       System.out.printf("Failed to move the file %s\n", filename);
     }
   }
 
-  public boolean checkFolder(String file) {
-    File folder = new File(file);
-    return folder.exists();
+  public void runFileManager() {
+    while (true) {
+      Scanner in = new Scanner(System.in);
+      String[] data = in.nextLine().split(" ");
+      String currentPath = _path.toString();
+      if (data[0].equals("exit")) {
+        break;
+      }
+      if (data[0].equals("cd")) {
+        currentPath = this.cdCommand(data[1]);
+      } else if (data[0].equals("ls")) {
+        this.lsCommand(currentPath);
+      } else if (data[0].equals("mv")) {
+        if (data[2].startsWith("../") || data[2].startsWith("/")) {
+          this.mvMove(currentPath, data[1], data[2]);
+        } else {
+          this.mvRename(currentPath, data[1], data[2]);
+
+        }
+      }
+    }
   }
+
 }
--- src/day02/ex02/Program.java
+++ src/day02/ex02/Program.java
@@ -7,27 +7,13 @@ public class Program {
 
   public static void main(String[] args) {
 
-    FileManagerService path = new FileManagerService();
-
-    while (true) {
-      Scanner in = new Scanner(System.in);
-      String[] data = in.nextLine().split(" ");
-      String currentPath = path._path.toString();
-      if (data[0].equals("exit")) {
-        break;
-      }
-      if (data[0].equals("cd")) {
-        currentPath = path.cdCommand(data[1]);
-      } else if (data[0].equals("ls")) {
-        path.lsCommand(currentPath);
-      } else if (data[0].equals("mv")) {
-        if (data[2].startsWith("../") || data[2].startsWith("/")) {
-          path.mvMove(currentPath, data[1], data[2]);
-        } else {
-          path.mvRename(currentPath, data[1], data[2]);
-
-        }
-      }
+    if (args.length < 1) {
+      System.out.println("a few arguments");
+    } else {
+      String[] argument = args[0].split("=");
+      FileManagerService fileManagerService = new FileManagerService(
+          argument[1]);
+      fileManagerService.runFileManager();
     }
   }
 }
