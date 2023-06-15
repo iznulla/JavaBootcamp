471b53278cf43940e0a5e0868be6045a9c075be3
Meryl Porsha <merylpor@un-k5.kzn.21-school.ru>
2023-06-15 23:26:05 +0300

    fix data.sql

--- src/day05/ex00/Chat/src/main/resources/data.sql
+++ src/day05/ex00/Chat/src/main/resources/data.sql
@@ -1,10 +1,25 @@
 
-INSERT INTO USERS VALUES(1, 'Ayfon', 'cornhub11');
-INSERT INTO USERS VALUES(2, 'Aypod', 'cornhub12');
-INSERT INTO USERS VALUES(3, 'Aypad', 'cornhub13');
-INSERT INTO USERS VALUES(4, 'Ayrat', 'cornhub14');
-INSERT INTO USERS VALUES(5, 'Aygul', 'cornhub15');
-INSERT INTO USERS VALUES(6, 'Aylin', 'cornhub16');
+INSERT INTO users (login, password, rooms, chats)
+VALUES ('Ayfon', 'cornhub11', ARRAY[1, 2, 4], ARRAY[1, 2, 3, 4]);
+INSERT INTO users (login, password, rooms, chats)
+VALUES ('Aypod', 'cornhub12', ARRAY[5], ARRAY[5]);
+INSERT INTO users (login, password, rooms, chats)
+VALUES ('Aypad', 'cornhub13', ARRAY[3], ARRAY[3, 2]);
+INSERT INTO users (login, password, chats)
+VALUES ('Ayrat', 'cornhub14', ARRAY[2]);
+INSERT INTO users (login, password, chats)
+VALUES ('Aygul', 'cornhub15', ARRAY[3]);
+INSERT INTO users (login, password, chats)
+VALUES ('Aylin', 'cornhub16', ARRAY[4]);
+-- INSERT INTO USERS VALUES(1, 'Ayfon', 'cornhub11');
+-- INSERT INTO USERS VALUES(2, 'Aypod', 'cornhub12');
+-- INSERT INTO USERS VALUES(3, 'Aypad', 'cornhub13');
+-- INSERT INTO USERS VALUES(4, 'Ayrat', 'cornhub14');
+-- INSERT INTO USERS VALUES(5, 'Aygul', 'cornhub15');
+-- INSERT INTO USERS VALUES(6, 'Aylin', 'cornhub16');
+
+
+
 
 INSERT INTO chatroom VALUES(1, 'school11', 1);
 INSERT INTO chatroom VALUES(2, 'school21', 1);
