9b1ff4bd341a83a19b69f1123f984ea596e21c6a
Meryl Porsha <merylpor@student.21-school.ru>
2023-07-04 17:57:30 +0300

    ex00complite

---
+++ src/day06/ex00/Tests/src/main/java/edu/school21/numbers/IllegalNumberExeption.java
@@ -0,0 +1,7 @@
+package edu.school21.numbers;
+
+public class IllegalNumberExeption extends RuntimeException{
+  IllegalNumberExeption(String message) {
+    super(message);
+  }
+}
---
+++ src/day06/ex00/Tests/src/main/java/edu/school21/numbers/NumberWorker.java
@@ -0,0 +1,26 @@
+package edu.school21.numbers;
+
+public class NumberWorker {
+
+  public NumberWorker() {}
+
+  public boolean isPrime(int number) {
+    if (number <= 1)
+      throw new IllegalNumberExeption("Illegal number");
+    for (int i = 2; i * i <= number; ++i) {
+      if (number % i == 0)
+        return false;
+    }
+    return true;
+  }
+
+  public int digitsSum(int number) {
+    number = Math.abs(number);
+    int sum = 0;
+    while (number > 0) {
+      sum += number % 10;
+      number /= 10;
+    }
+    return sum;
+  }
+}
