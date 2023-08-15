4c6f3eae828697ee3979b16ceb4f04348032c7b0
Meryl Porsha <merylpor@un-k5.kzn.21-school.ru>
2023-08-15 18:00:09 +0300

    ex02

---
+++ src/day08/ex02/Service/src/main/java/school21/spring/service/repositories/UserMapper.java
@@ -0,0 +1,18 @@
+package school21.spring.service.repositories;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+import school21.spring.service.models.User;
+
+public class UserMapper implements RowMapper<User> {
+
+  @Override
+  public User mapRow(ResultSet resultSet, int i) throws SQLException {
+    final int id = resultSet.getInt("id");
+    final String email = resultSet.getString("email");
+    User user = new User(email);
+    user.setIdentifier(Long.parseLong(String.valueOf(id)));
+    return user;
+  }
+}
